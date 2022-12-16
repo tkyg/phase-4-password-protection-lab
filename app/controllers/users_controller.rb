f9ef5adc6a134e38443d172792d307f017045b04
tkyg <80114433+tkyg@users.noreply.github.com>
2022-12-16 13:51:59 -0500

    done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find_by(id: sessions[:user_id])
+    render json: user
+  end
+  
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
+end
